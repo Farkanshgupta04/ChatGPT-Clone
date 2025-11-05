be36c9192a5b65a55a3a65d883ef9f6ea7dca8a4
Farkansh Gupta <farkanshgupta@gmail.com>
2025-11-05 15:49:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());//frontend mai use kar payenge iss se 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "https://chatgpt-clone-navy-seven.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
